91e0d09572ceb0f0a7bdc4c5e05936ebe5dc9405
MayfieldITKY <142246051+MayfieldITKY@users.noreply.github.com>
2023-08-14 15:12:46 -0400

    Create AutoUpdateTaskScripts.ps1

---
+++ SetupScripts/AutoUpdateTaskScripts.ps1
@@ -0,0 +1,14 @@
+# Checks for new or updated scripts from GitHub repository.
+# Downloads updates and creates new Scheduled Tasks if needed.
+
+# Check for updates
+    # Download GitHub files to temp folder
+    # Check against current files
+    # Replaces or creates scripts
+
+# Check for Scheduled Tasks
+    # Get updated list of tasks from GitHub
+    # Check against current Scheduled Tasks
+    # Create new tasks if needed
+
+# Remove temporary files
