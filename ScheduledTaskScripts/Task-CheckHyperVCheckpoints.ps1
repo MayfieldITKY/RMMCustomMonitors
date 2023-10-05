f47811bf5417ae78d65f2ceab7e9ea6d7b53cb13
MayfieldITKY <support@mitky.com>
2023-10-05 08:58:32 -0400

    Corrections, WSB Revisions

--- ScheduledTaskScripts/Task-CheckHyperVCheckpoints.ps1
+++ ScheduledTaskScripts/Task-CheckHyperVCheckpoints.ps1
@@ -1,6 +1,6 @@
-# Check for Hyper-V Checkpoints on server hosts. Excludes RDC and TESTING VMs
-# Runs as a scheduled task and writes status to Event Log
-# Connectwise monitors this log and alerts if checkpoints are found
+# Checks for Hyper-V Checkpoints on server hosts. Excludes RDC and TESTING VMs.
+# Runs as a scheduled task and writes status to Event Log.
+# RMM monitors this log and alerts if checkpoints are found.
 
 $checkpoints = Get-VM * | Where-Object{$_.Name -notlike "*RDC*" -and $_.Name -notlike "*test*"} | ForEach-Object {Get-VMCheckpoint -VMName $_.Name}
 
