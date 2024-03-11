c4837964ae575677ab9e1fa702711b9fbb54d118
MayfieldITKY <99356397+MITKY-Jason@users.noreply.github.com>
2024-03-11 12:27:04 -0400

    update files

--- SetupScripts/CreateMITKYEventLog.ps1
+++ SetupScripts/CreateMITKYEventLog.ps1
@@ -3,5 +3,5 @@
 if (Get-WinEvent -LogName MITKY) {
     Write-Output "Event Log for MITKY already exists."
 } else {
-    New-EventLog -LogName MITKY -Source 'Scheduled Tasks', 'RMM'
+    New-EventLog -LogName MITKY -Source 'Scheduled Tasks', 'Maintenance Tasks', 'RMM'
 }
