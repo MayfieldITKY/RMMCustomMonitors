fd8782a1d2e55d27f7b4bfc14c858afba182a036
MayfieldITKY <99356397+MITKY-Jason@users.noreply.github.com>
2024-03-19 16:59:34 -0400

    Update Create-CheckForShadowCopies.ps1

--- SetupScripts/CreateScheduledTasks/Create-CheckForShadowCopies.ps1
+++ SetupScripts/CreateScheduledTasks/Create-CheckForShadowCopies.ps1
@@ -3,6 +3,6 @@
 $TaskName = "MITKY - Check for Shadow Copies"
 $Trigger = New-ScheduledTaskTrigger -At 7:00am -Daily
 $User = "NT AUTHORITY\SYSTEM"
-$Action = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "C:\Scripts\CustomMonitors\ScheduledTaskScripts\CheckForShadowCopies.ps1"
+$Action = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "C:\Scripts\RMMCustomMonitors\ScheduledTaskScripts\CheckForShadowCopies.ps1"
 
 Register-ScheduledTask -TaskName $TaskName -Trigger $Trigger -User $User -Action $Action
