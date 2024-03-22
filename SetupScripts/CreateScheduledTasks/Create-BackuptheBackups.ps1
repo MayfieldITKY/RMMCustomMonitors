5697b80e64eaa992ed965db2e4c94b8c697003d9
MayfieldITKY <99356397+MITKY-Jason@users.noreply.github.com>
2024-03-22 15:32:22 -0400

    Update Create-BackuptheBackups.ps1

--- SetupScripts/CreateScheduledTasks/Create-BackuptheBackups.ps1
+++ SetupScripts/CreateScheduledTasks/Create-BackuptheBackups.ps1
@@ -1,26 +1,29 @@
 # ====================== CREATE TASK: BACKUP THE BACKUPS ======================
 # Create or update a scheduled task to run Maintenance-BackuptheBackups.ps1.
 
+
 # First look for an existing task using the old batch script and disable it
+$oldBackupTaskNames = @("backup the backups", "backup_the_backups", "backupthebackups")
+Get-ScheduledTask | Where-Object {$oldBackupTaskNames -contains $_.TaskName} | Disable-ScheduledTask
 
 
 # Check for an existing task and delete it if found. This is needed in case 
 # Windows Server Backup schedule has changed since the last update.
-$wsbStartTime = ""
-$taskTriggerTime = ""
+$TaskName = "MITKY - Backup the Backups"
+Unregister-ScheduledTask -TaskName $TaskName -Confirm:$false -ErrorAction Ignore
 
 
 # Get Windows Server Backup schedule and set the task to run 30 minutes before
-
-
-
+$wsbPolicy = Get-WBPolicy
+$wsbTime = Get-WBSchedule -Policy $wsbPolicy
+$taskTriggerTime = $wsbTime.AddMinutes(-30)
+$taskTriggerTime = $taskTriggerTime.ToString("HH:mm")
 
 
 # Create the new task
-
 $TaskName = "MITKY - Backup the Backups"
-$Trigger = New-ScheduledTaskTrigger -At $taskTriggerTime -Daily
+$taskTrigger = New-ScheduledTaskTrigger -At $taskTriggerTime -Daily
 $User = "NT AUTHORITY\SYSTEM"
 $Action = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "C:\Scripts\RMMCustomMonitors\MaintenanceTaskScripts\BackuptheBackups.ps1"
 
-Register-ScheduledTask -TaskName $TaskName -Trigger $Trigger -User $User -Action $Action
+Register-ScheduledTask -TaskName $TaskName -Trigger $taskTrigger -User $User -Action $Action -RunLevel Highest
