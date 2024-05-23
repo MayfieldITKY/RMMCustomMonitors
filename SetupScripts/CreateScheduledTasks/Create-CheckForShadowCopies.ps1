faeb6a0442122e991267343591645c141d498d07
MayfieldITKY <99356397+MITKY-Jason@users.noreply.github.com>
2024-05-23 12:52:43 -0400

    replace test files

--- SetupScripts/CreateScheduledTasks/Create-CheckForShadowCopies.ps1
+++
@@ -1,8 +0,0 @@
-# Creates scheduled task to check for shadow copies on server host
-
-$TaskName = "MITKY - Check for Shadow Copies"
-$Trigger = New-ScheduledTaskTrigger -At 7:00am -Daily
-$User = "NT AUTHORITY\SYSTEM"
-$Action = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "C:\Scripts\RMMCustomMonitors\ScheduledTaskScripts\CheckForShadowCopies.ps1"
-
-Register-ScheduledTask -TaskName $TaskName -Trigger $Trigger -User $User -Action $Action
