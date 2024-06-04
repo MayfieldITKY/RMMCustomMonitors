2d2160bd07f2c03888f7bb377a036861bfe255cf
MayfieldITKY <99356397+MITKY-Jason@users.noreply.github.com>
2024-06-04 11:22:27 -0400

    disable tests, add shadow copy scripts

---
+++ MaintenanceTaskScripts/Task-CheckandClearShadowCopies.ps1
@@ -0,0 +1,110 @@
+﻿
+# ============== MAINTENANCE TASK: CHECK AND CLEAR SHADOW COPIES ==============
+<#
+Checks for shadow copies on a HOST server and attempts to delete them by
+resizing shadow copy storage. The script will find all volumes with shadow copies
+and continue trying to clear them unless the disk space used by shadow copies is
+not decreasing after an attempt. It will write results to the MITKY event log,
+and RMM will monitor and alert only if shadow copies exist and could not be cleared.
+#>
+# =============================================================================
+
+# Define all needed functions
+Function Get-VolumesWithShadows () {
+    $volumes = @()
+    $allShadowCopies = Get-CimInstance -Class "Win32_ShadowCopy"
+    foreach ($copy in $allShadowCopies) {
+        if (-not ($volumes -contains $copy.VolumeName)) {$volumes += $copy.VolumeName}
+    }
+    $volumes
+}
+
+Function Get-DriveLetter ($volume) {
+    $v = Get-CimInstance win32_volume | Where-Object {$_.DeviceID -eq $volume}
+    $v.Name
+}
+
+Function Get-ShadowSpace ($volume) {
+    $spaceUsed = Get-CimInstance -Class Win32_ShadowStorage | Where-Object {$_.Volume.DeviceID -eq $volume} | Select-Object UsedSpace
+    $spaceUsed
+}
+
+Function Get-VolumesToClear ($volume) {
+    $usedSpace = Get-ShadowSpace $volume | Select-Object -ExpandProperty UsedSpace
+    $volumeData = @{ID = $volume; Letter = (Get-DriveLetter $volume); UsedSpace = $usedSpace}
+    $volumeData
+}
+
+Function Remove-ShadowCopies ($drive) {
+    $commandRemove = "vssadmin resize shadowstorage /for=$drive /on=$drive /maxsize=320MB"
+    $commandUnbound = "vssadmin resize shadowstorage /for=$drive /on=$drive /maxsize=unbounded"
+    cmd.exe /c $commandRemove
+    cmd.exe /c $commandUnbound
+}
+
+# =============================================================================
+
+# Check if shadow copies exist. If any, get volumes with shadows and relevant data
+if (-not (Get-CimInstance -Class "Win32_ShadowCopy" -ErrorAction Ignore)) {
+    # No shadow copies, so write to event log and exit
+    $params = @{
+        LogName = "MITKY"
+        Source = "Scheduled Tasks"
+        EntryType = "Information"
+        EventId = 1069
+        Message = "No shadow copies were found on host server."
+    }
+    Write-EventLog @params
+    exit
+}
+
+$volumesToClear = @()
+foreach ($vol in Get-VolumesWithShadows) {$volumesToClear += Get-VolumesToClear $vol}
+
+# For each volume, get drive letter and attempt to clear shadow copies
+$allShadowsCleared = $false
+
+foreach ($vol in $volumesToClear) {
+    $letter = $vol.Letter
+    $usedSpaceBefore = $vol.UsedSpace
+
+    # Initial clear attempt
+    Remove-ShadowCopies $letter
+    $usedSpaceAfter = Get-ShadowSpace $vol | Select-Object -ExpandProperty UsedSpace
+
+    # Additional attempts if needed. Stop if used space does not reduce after an attempt
+    if ($usedSpaceAfter -ne 0) {
+        While ($usedSpaceBefore -gt $usedSpaceAfter) {
+            $usedSpaceBefore = $usedSpaceAfter
+            Remove-ShadowCopies $letter
+            $usedSpaceAfter = Get-ShadowSpace $vol | Select-Object -ExpandProperty UsedSpace
+            if ($usedSpaceAfter -eq 0) {
+                break
+            }
+        }
+    }
+}
+
+# Check that all shadows were cleared for all volumes and report results to event log
+if (-not (Get-CimInstance -Class "Win32_ShadowCopy")) {$allShadowsCleared = $true}
+
+if ($allShadowsCleared) {
+    $params = @{
+        LogName = "MITKY"
+        Source = "Scheduled Tasks"
+        EntryType = "Information"
+        EventId = 1068
+        Message = "Shadow copies were found on host server and successfully cleared."
+    }
+    Write-EventLog @params
+} else {
+    $params = @{
+        LogName = "MITKY"
+        Source = "Scheduled Tasks"
+        EntryType = "Information"
+        EventId = 1061
+        Message = "Shadow copies were found on host server and could not be cleared!"
+    }
+    Write-EventLog @params
+}
+    
---
+++ SetupScripts/CreateScheduledTasks/Create-CheckandClearShadowCopies.ps1
@@ -0,0 +1,61 @@
+﻿# COPY this template script for creating new monitoring and maintenance tasks.
+
+# ====================== CREATE TASK: TASK NAME ======================
+# Create or update a scheduled task to run <Task script>.ps1.
+
+# USE THIS TO DISABLE ANY OBSOLETE TASKS. COMMENT IF NOT NEEDED.
+# First look for an existing task using the old batch script and disable it
+# $oldTaskNames = @("possible task name", "other task name", "task_name_could_be_this")
+# Get-ScheduledTask | Where-Object {$oldTaskNames -contains $_.TaskName} | Disable-ScheduledTask
+
+# THIS IS ALWAYS NEEDED.
+# Check for an existing task and delete it if found. This is needed in case 
+# task schedule or other parameters have changed since the last update.
+$taskName = "MITKY - <TASK NAME>"
+Unregister-ScheduledTask -TaskName $TaskName -Confirm:$false -ErrorAction Ignore
+
+# USE THIS SECTION FOR PARAMETERS THAT MUST BE GENERATED PROGRAMMATICALLY.
+# EXAMPLE: SCHEDULING AROUND WINDOWS SERVER BACKUP SCHEDULE.
+# $wsbPolicy = Get-WBPolicy
+# $wsbTime = Get-WBSchedule -Policy $wsbPolicy
+# $taskTriggerTime = $wsbTime.AddMinutes(-30)
+# $taskTriggerTime = $taskTriggerTime.ToString("HH:mm")
+
+# SPECIFY THESE VARIABLES
+$pathToScript = "C:\FULL\PATH\TO\SCRIPT.ps1"
+$newTaskName = "MITKY - <TASK NAME>"
+$taskTriggerTime = "HH:mm"
+$taskTrigger = -At $taskTriggerTime -Daily # Other trigger types can be used
+
+# DO NOT CHANGE THESE VARIABLES
+$arguments = "-NoProfile -NoLogo -NonInteractive -ExecutionPolicy Bypass -File $pathToScript"
+$User = "NT AUTHORITY\SYSTEM"
+$Action = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument $arguments
+$taskPath = "Mayfield IT"
+
+# CREATES THE SCHEDULED TASK
+Register-ScheduledTask -TaskName $newTaskName -Trigger $taskTrigger -User $User -Action $Action -RunLevel Highest -TaskPath $taskPath
+
+# Checks that the task was created successfully and is active, and write the 
+# result to the event log. An error should trigger an alert from an RMM monitor.
+$newTask = Get-ScheduledTask -TaskName $newTaskName
+
+if ($newTask.State -eq "Ready") {
+  $params = @{
+    LogName = "MITKY"
+    Source = "RMM"
+    EntryType = "Information"
+    EventId = 109
+    Message = "Task $newTaskName was created or updated successfully."
+  }
+  Write-EventLog @params
+} else {
+  $params = @{
+    LogName = "MITKY"
+    Source = "RMM"
+    EntryType = "Error"
+    EventId = 102
+    Message = "Updating or creating task $newTaskName failed!"
+  }
+  Write-EventLog @params
+}
