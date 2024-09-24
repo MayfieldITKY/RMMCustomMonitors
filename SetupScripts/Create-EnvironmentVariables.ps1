c3c5fac03b06748292f506fec2ae3cee442780f9
MITKY-Jason <99356397+MITKY-Jason@users.noreply.github.com>
2024-09-24 16:58:08 -0400

    Create Create-EnvironmentVariables.ps1

---
+++ SetupScripts/Create-EnvironmentVariables.ps1
@@ -0,0 +1,11 @@
+# ======================== UPDATE ENVIRONMENT VARIABLES =======================
+# Sets or updates environment variables used by other monitoring scripts and reporting.
+# These should be client- or computer-specific values that cannot be easily found
+# programmatically, such as site variables in Datto RMM. These should NEVER include
+# any sort of secret: passwords, certificate keys, API keys, etc.
+
+# DO NOT USE ENVIRONMENT VARIABLES FOR STORING SECRETS!
+
+# Datto RMM variables:
+
+
