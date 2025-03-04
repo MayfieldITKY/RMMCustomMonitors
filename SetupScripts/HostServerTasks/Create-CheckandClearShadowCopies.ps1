e58451887148daa4501ce7b4ce626d2fea8ba16a
MITKY-Jason <99356397+MITKY-Jason@users.noreply.github.com>
2025-03-04 11:57:13 -0500

    Update Create-CheckandClearShadowCopies.ps1

--- SetupScripts/HostServerTasks/Create-CheckandClearShadowCopies.ps1
+++ SetupScripts/HostServerTasks/Create-CheckandClearShadowCopies.ps1
@@ -1,10 +1,6 @@
 ﻿# ================= CREATE TASK: CHECK AND CLEAR SHADOW COPIES ================
 # Create or update a scheduled task to run HOST-CheckandClearShadowCopies.ps1.
 
-# First look for an existing task using the old batch script and disable it
-# $oldTaskNames = @("possible task name", "other task name", "task_name_could_be_this")
-# Get-ScheduledTask | Where-Object {$oldTaskNames -contains $_.TaskName} | Disable-ScheduledTask
-
 # Check for an existing task and delete it if found. This is needed in case 
 # task schedule or other parameters have changed since the last update.
 $taskName = "MITKY - Check and Clear Shadow Copies"
@@ -13,7 +9,7 @@ Unregister-ScheduledTask -TaskName $TaskName -Confirm:$false -ErrorAction Ignore
 # SPECIFY THESE VARIABLES - Schedule 30 minutes before Windows Server Backup
 $taskTriggerTime = "19:30" # Default to 7:30 PM if there is no other schedule
 # If Windows Server Backup has previously run, use that time
-if (Get-WBJob -Previous 1) {
+if ((Get-WBPolicy) -and (Get-WBJob -Previous 1)) {
     $lastBackupTime = [datetime]$((Get-WBJob -Previous 1 | Select-Object -Property StartTime).StartTime)
     $taskTriggerTime = $($lastBackupTime.AddMinutes(-30)).ToString("HH:mm")
 } 
