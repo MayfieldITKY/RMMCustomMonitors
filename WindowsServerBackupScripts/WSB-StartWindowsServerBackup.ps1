69827142e4349414e6d7334033134fb7140b4c03
MITKY-Jason <99356397+MITKY-Jason@users.noreply.github.com>
2025-04-25 10:48:53 -0400

    Update WSB-StartWindowsServerBackup.ps1

--- WindowsServerBackupScripts/WSB-StartWindowsServerBackup.ps1
+++ WindowsServerBackupScripts/WSB-StartWindowsServerBackup.ps1
@@ -7,9 +7,8 @@ $lastBackupTarget = $(Get-WBSummary | Select-Object -Property LastBackupTarget).
 if (-not ($lastBackupTarget)) {
     $lastBackupTarget = Get-PSDrive | Where-Object {($_.Provider -like "*FileSystem") -and (($_.Description -like "*Backup*") -or ($_.Description -like "*WSB*"))}
     if (-not ($lastBackupTarget)) {exit 1}
-}
-
-$backupLocation = New-WBBackupTarget -VolumePath $lastBackupTarget.Root
+    $Script:backupLocation = New-WBBackupTarget -VolumePath $lastBackupTarget.Root
+} else {$Script:backupLocation = New-WBBackupTarget -VolumePath $lastBackupTarget}
 
 Add-WBBackupTarget -Policy $policy -Target $backupLocation
 Add-WBVirtualMachine -Policy $policy -VirtualMachine $VMs
