1dce10e026299785f096a37eef16d30f91e0f3aa
MITKY-Jason <99356397+MITKY-Jason@users.noreply.github.com>
2025-07-11 12:20:20 -0400

    Update Create-CheckandClearShadowCopies.ps1

--- SetupScripts/HostServerTasks/Create-CheckandClearShadowCopies.ps1
+++ SetupScripts/HostServerTasks/Create-CheckandClearShadowCopies.ps1
@@ -13,10 +13,7 @@ Unregister-ScheduledTask -TaskName $TaskName -Confirm:$false -ErrorAction Ignore
 # SPECIFY THESE VARIABLES - Schedule 30 minutes before Windows Server Backup
 $taskTriggerTime = "19:30" # Default to 7:30 PM if there is no other schedule
 # If Windows Server Backup has previously run, use that time
-if (Get-WBJob -Previous 1) {
-    $lastBackupTime = [datetime]$((Get-WBJob -Previous 1 | Select-Object -Property StartTime).StartTime)
-    $taskTriggerTime = $($lastBackupTime.AddMinutes(-30)).ToString("HH:mm")
-} 
+if ($env:clear_shadow_copies_time) {$taskTriggerTime = $env:clear_shadow_copies_time} 
 $pathToScript = "C:\Scripts\RMMCustomMonitors\HostServerScripts\HOST-CheckandClearShadowCopies.ps1"
 $newTaskName = "MITKY - Check and Clear Shadow Copies"
 $taskTrigger = New-ScheduledTaskTrigger -At $taskTriggerTime -Daily
