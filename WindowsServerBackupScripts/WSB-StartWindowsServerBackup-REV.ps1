5391bbe83eba6e33d2ab0bf6e1d24dda1ce31102
MITKY-Jason <99356397+MITKY-Jason@users.noreply.github.com>
2025-07-18 16:52:50 -0400

    Update WSB-StartWindowsServerBackup-REV.ps1

--- WindowsServerBackupScripts/WSB-StartWindowsServerBackup-REV.ps1
+++ WindowsServerBackupScripts/WSB-StartWindowsServerBackup-REV.ps1
@@ -4,11 +4,10 @@
 $hostname = $env:COMPUTERNAME
 $client = $env:ShortSiteName
 
-
 function Get-Timestamp {Get-Date -Format "MM/dd/yyyy HH:mm:ss"}
 
 # Create the log file
-function New-UpdateLogFile {
+function New-LogFile {
     Param(
         [Parameter(Mandatory= $false)]
         [string]$updateLogName = "StartWindowsServerBackup",
@@ -26,7 +25,7 @@ function New-UpdateLogFile {
 }
 
 # Write to log file and output
-function Write-UpdateLogAndOutput {
+function Write-LogAndOutput {
     Param(
         [Parameter(Mandatory= $false)]
         [string]$message,
@@ -49,21 +48,46 @@ function Write-UpdateLogAndOutput {
 
 
 # Start a log file
-
+New-LogFile
+Write-LogAndOutput @"
+=================== SCHEDULED TASK: START WINDOWS SERVER BACKUP ===============
+$(Get-Date | Out-String)
+"@ -NoTimestamp
 # First check for a backup policy defined by Windows Server Backup and remove it
-if (Get-WBPolicy) {Remove-WBPolicy -All -Force -ErrorAction Ignore}
+if (Get-WBPolicy) {
+    Write-LogAndOutput "Removing previously scheduled backup policy..."
+    Remove-WBPolicy -All -Force -ErrorAction Ignore
+} else {Write-LogAndOutput "No scheduled backup policy found."}
+Write-LogAndOutput
 
+# Create backup policy
 $policy = New-WBPolicy
 # Get all Hyper-V VMs, excluding any that are named with "test" or "-dbu" (for don't backup)
+Write-LogAndOutput "Looking for virtual machines..."
 $VMs = Get-WBVirtualMachine | Where-Object {($_.VMName -notlike "*test*") -and ($_.VMName -notlike "*-dbu")}
+If ($VMs) {
+    Write-LogAndOutput "The following VMs will be included in the backup:"
+    foreach ($vm in $VMs) {Write-LogAndOutput "$($vm.VMName)" -NoTimestamp}
+} else {
+    Write-LogAndOutput "No VMs were found! Confirm this is running on a Hyper-V host and VMs are set up."
+    Write-LogAndOutput "Exiting..."
+    exit 1
+} Write-LogAndOutput
+
 # Find the last used backup drive or try to find a suitable drive
+Write-LogAndOutput "Determining backup location..."
 $lastBackupTarget = $(Get-WBSummary | Select-Object -Property LastBackupTarget).LastBackupTarget
 if (-not ($lastBackupTarget)) {
     $lastBackupTarget = Get-PSDrive | Where-Object {($_.Provider -like "*FileSystem") -and (($_.Description -like "*Backup*") -or ($_.Description -like "*WSB*"))}
     if (-not ($lastBackupTarget)) {exit 1}
     $Script:backupLocation = New-WBBackupTarget -VolumePath $lastBackupTarget.Root
 } else {$Script:backupLocation = New-WBBackupTarget -VolumePath $lastBackupTarget}
+Write-LogAndOutput "Backup will be sent to $backupLocation"
+Write-LogAndOutput
 
 Add-WBBackupTarget -Policy $policy -Target $backupLocation
 Add-WBVirtualMachine -Policy $policy -VirtualMachine $VMs
-Start-WBBackup -Policy $policy
+Write-LogAndOutput "Starting Windows Server Backup..."
+Write-LogAndOutput
+$backupReport = Start-WBBackup -Policy $policy
+Write-LogAndOutput $backupReport -NoTimestamp
